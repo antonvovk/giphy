0320dcdd3c025c5cd60e61021a9451287511b864
AntonVovk <archwolf@protonmail.com>
2019-11-26 23:26:07 +0200

    fds

--- frontend/src/app/modules/color-models/color-models.component.ts
+++ frontend/src/app/modules/color-models/color-models.component.ts
@@ -1,6 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ColorModel} from '../../services/color-models/color-model';
 import {MatBottomSheet} from "@angular/material/bottom-sheet";
+import {ColorModel} from "../../services/color-models/color-model";
 import {BottomSheetComponent} from "../popups/bottom-sheet";
 
 @Component({
@@ -11,18 +11,18 @@ import {BottomSheetComponent} from "../popups/bottom-sheet";
 export class ColorModelsComponent implements OnInit {
 
   selectedFile = null;
+  brightness = 0;
+
+  imgRGB: ImageData;
+  image: HTMLImageElement;
 
   R = 0;
   G = 0;
   B = 0;
-  X = 0;
-  Y = 0;
-  Z = 0;
-  brightness = 0;
-  contrast = 0;
-  colorSpace = 'RGB';
 
-  private imageDataCopy: ImageData;
+  X = '';
+  Y = '';
+  Z = '';
 
   @ViewChild('canvas', {static: false}) canvas: ElementRef<HTMLCanvasElement>;
   @ViewChild('mainContent', {static: false}) mainContent: ElementRef;
@@ -30,6 +30,7 @@ export class ColorModelsComponent implements OnInit {
   constructor(private bottomSheet: MatBottomSheet) { }
 
   ngOnInit() {
+
   }
 
   onFileChanged(event) {
@@ -41,75 +42,82 @@ export class ColorModelsComponent implements OnInit {
     const ctx = this.canvas.nativeElement.getContext('2d');
     ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
 
-    const img = new Image();
-    img.onload = () => {
-      this.canvas.nativeElement.width = img.width;
-      this.canvas.nativeElement.height = img.height;
-      ctx.drawImage(img, 0, 0);
-      this.imageDataCopy = ctx.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    const image = new Image();
+    image.src = URL.createObjectURL(this.selectedFile);
+
+    image.onload = () => {
+      this.canvas.nativeElement.width = image.width * 2;
+      this.canvas.nativeElement.height = image.height * 2;
+      this.imgRGB = this.getImageData(image);
+      this.image = image;
+      ctx.putImageData(this.imgRGB, 0, 0);
+      ctx.putImageData(ColorModel.convertToXYZ(this.imgRGB), image.width, 0);
     };
-
-    img.src = URL.createObjectURL(this.selectedFile);
   }
 
-  onImageChaneRGB() {
-    const ctx = this.canvas.nativeElement.getContext('2d');
-    const imageData = ctx.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-    //ColorModel.applyRGB(imageData, this.R, this.G, this.B);
-    ctx.putImageData(imageData, 0, 0);
+  getImageData(image: HTMLImageElement): ImageData {
+    const tmpCanvas = document.createElement('canvas');
+    const ctx = tmpCanvas.getContext('2d');
+    tmpCanvas.width = image.width;
+    tmpCanvas.height = image.height;
+    ctx.drawImage(image, 0, 0);
+    return ctx.getImageData(0, 0, image.width, image.height);
   }
 
-  onImageChaneXYZ() {
-    const ctx = this.canvas.nativeElement.getContext('2d');
-    const imageData = ctx.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-    //ColorModel.applyXYZ(imageData, this.X, this.Y, this.Z);
-    ctx.putImageData(imageData, 0, 0);
-  }
-
-  onImageChangeBrightness() {
-    const ctx = this.canvas.nativeElement.getContext('2d');
-    const imageData = ctx.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-    //ColorModel.applyBrightness(imageData, this.brightness);
-    ctx.putImageData(imageData, 0, 0);
-  }
-
-  onImageReset() {
-    this.onUpload();
-
-    if (this.colorSpace === 'RGB') {
-      this.R = 0;
-      this.G = 0;
-      this.B = 0;
-    } else {
-      this.X = 0;
-      this.Y = 0;
-      this.Z = 0;
+  findPos(obj) {
+    let curleft = 0, curtop = 0;
+    if (obj.offsetParent) {
+      do {
+        curleft += obj.offsetLeft;
+        curtop += obj.offsetTop;
+      } while (obj == obj.offsetParent);
+      return {x: curleft, y: curtop};
     }
-
-    this.brightness = 0;
-    this.contrast = 0;
+    return undefined;
   }
 
-  onImageChangeContrast() {
+  mousemove(event) {
+    const pos = this.findPos(this.canvas.nativeElement);
+    const x = event.clientX - pos.x;
+    const y = event.clientY - pos.y;
+    const p = this.canvas.nativeElement.getContext('2d').getImageData(x, y, 1, 1).data;
+    this.R = p[0];
+    this.G = p[1];
+    this.B = p[2];
+
+    const xyz = ColorModel.convertRGBtoXYZ(p[0], p[1], p[2]);
+    this.X = xyz[0].toFixed(4);
+    this.Y = xyz[1].toFixed(4);
+    this.Z = xyz[2].toFixed(4);
+  }
+
+  brightnessChanged() {
     const ctx = this.canvas.nativeElement.getContext('2d');
-    const imageData = ctx.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-    ColorModel.applyContrast(imageData, this.contrast);
-    ctx.putImageData(imageData, 0, 0);
+    ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    this.canvas.nativeElement.width = this.imgRGB.width * 2;
+    this.canvas.nativeElement.height = this.imgRGB.height * 2;
+    this.imgRGB = this.getImageData(this.image);
+    ctx.putImageData(ColorModel.applyBrightness(this.imgRGB, this.brightness), 0, 0);
+    ctx.putImageData(ColorModel.convertToXYZ(ColorModel.applyBrightness(this.imgRGB, this.brightness)), this.imgRGB.width, 0);
   }
 
+  saveTrigered() {
+    const tmpCanvas = document.createElement('canvas');
+    const ctx = tmpCanvas.getContext('2d');
+    tmpCanvas.width = this.image.width;
+    tmpCanvas.height = this.image.height;
+    ctx.putImageData(ColorModel.convertToXYZ(this.getImageData(this.image)), 0, 0);
 
-
-  onSaveImage() {
-    this.bottomSheet.open(BottomSheetComponent);
-    //const dataURL = this.canvas.nativeElement.toDataURL('image/png');
-    //this.downloadImage(dataURL, 'graphify.png');
+    this.bottomSheet.open(BottomSheetComponent, {
+      data: {
+        canvas: tmpCanvas,
+        filename: this.selectedFile.name + 'XYZ'
+      }
+    });
   }
 
-  downloadImage(data, filename) {
-    const a = document.createElement('a');
-    a.href = data;
-    a.download = filename;
-    document.body.appendChild(a);
-    a.click();
+  resetTriggered() {
+    this.brightness = 0;
+    this.brightnessChanged();
   }
 }
--- frontend/src/app/services/color-models/color-model.ts
+++ frontend/src/app/services/color-models/color-model.ts
@@ -1,83 +1,28 @@
 export class ColorModel {
 
-  public static applyBrightness(imageData: ImageData, brightness: number): void {
-    for (let i = 0; i < imageData.data.length; i += 4) {
-      imageData[i] = imageData.data[i] + (255 * (brightness / 100));
-      imageData[i + 1] = imageData.data[i + 1] + (255 * (brightness / 100));
-      imageData[i + 2] = imageData.data[i + 2] + (255 * (brightness / 100));
-    }
-  }
-
-  public static applyContrast(imageData: ImageData, contrast: number): void {
-    const factor = (259.0 * (contrast + 255.0)) / (255.0 * (259.0 - contrast));
+  public static applyBrightness(imageDataSrc: ImageData, brightness: number): ImageData {
+    let imageData = imageDataSrc;
 
     for (let i = 0; i < imageData.data.length; i += 4) {
-      imageData[i] = this.truncateColor(factor * (imageData.data[i] - 128.0) + 128.0);
-      imageData[i + 1] = this.truncateColor(factor * (imageData.data[i + 1] - 128.0) + 128.0);
-      imageData[i + 2] = this.truncateColor(factor * (imageData.data[i + 2] - 128.0) + 128.0);
+      imageData.data[i] = imageData.data[i] + (255 * (brightness / 100));
+      imageData.data[i + 1] = imageData.data[i + 1] + (255 * (brightness / 100));
+      imageData.data[i + 2] = imageData.data[i + 2] + (255 * (brightness / 100));
     }
+
+    return imageData;
   }
 
-  public static applyRGB(imageData: ImageData, R: number, G: number, B: number): void {
+  public static convertToXYZ(imageDataSrc: ImageData): ImageData {
+    let imageData = imageDataSrc;
+
     for (let i = 0; i < imageData.data.length; i += 4) {
-      imageData[i] = this.truncateColor(imageData.data[i] + R);
-      imageData[i + 1] = this.truncateColor(imageData.data[i + 1] + G);
-      imageData[i + 2] = this.truncateColor(imageData.data[i + 2] + B);
-    }
-  }
-
-  public static applyXYZ(imageData: ImageData, X: number, Y: number, Z: number): void {
-    for (let i = 0; i < imageData.data.length; i += 4) {
-      const XYZ = this.convertRGBtoXYZ(imageData.data[i], imageData.data[i + 1], imageData.data[i + 2]);
-      const newX = this.truncateX(X + XYZ[0]);
-      const newY = this.truncateY(Y + XYZ[1]);
-      const newZ = this.truncateZ(Z + XYZ[2]);
-      const RGB = this.convertXYZtoRGB(newX, newY, newZ);
-
-      imageData[i] = RGB[0];
-      imageData[i + 1] = RGB[1];
-      imageData[i + 2] = RGB[2];
-    }
-  }
-
-  public static truncateColor(value) {
-    if (value < 0) {
-      value = 0;
-    } else if (value > 255) {
-      value = 255;
+      const xyz = this.convertRGBtoXYZ(imageData.data[i], imageData.data[i + 1], imageData.data[i + 2]);
+      imageData.data[i] = xyz[0] * 100.0 * 2.2;
+      imageData.data[i + 1] = xyz[1] * 100.0 * 2.2;
+      imageData.data[i + 2] = xyz[2] * 100.0 * 2.2;
     }
 
-    return value;
-  }
-
-  public static truncateX(value) {
-    if (value < 0) {
-      value = 0;
-    } else if (value > 95.05) {
-      value = 95.05;
-    }
-
-    return value;
-  }
-
-  public static truncateY(value) {
-    if (value < 0) {
-      value = 0;
-    } else if (value > 100) {
-      value = 100;
-    }
-
-    return value;
-  }
-
-  public static truncateZ(value) {
-    if (value < 0) {
-      value = 0;
-    } else if (value > 108.88) {
-      value = 108.88;
-    }
-
-    return value;
+    return imageData;
   }
 
   public static convertRGBtoXYZ(R: number, G: number, B: number) {
@@ -101,46 +46,10 @@ export class ColorModel {
       varB = varB / 12.92;
     }
 
-    varR = varR * 100;
-    varG = varG * 100;
-    varB = varB * 100;
-
-    const X = varR * 0.4124 + varG * 0.3576 + varB * 0.1805;
-    const Y = varR * 0.2126 + varG * 0.7152 + varB * 0.0722;
-    const Z = varR * 0.0193 + varG * 0.1192 + varB * 0.9505;
+    let X = (varR * 0.4124564 + varG * 0.3575761 + varB * 0.1804375);
+    let Y = (varR * 0.2126729 + varG * 0.7151522 + varB * 0.0721750);
+    let Z = (varR * 0.0193339 + varG * 0.1191920 + varB * 0.9503041);
 
     return [X, Y, Z];
-  }
-
-  public static convertXYZtoRGB(X: number, Y: number, Z: number) {
-    const varX = X / 100;
-    const varY = Y / 100;
-    const varZ = Z / 100;
-
-    let varR = varX * 3.2406 + varY * -1.5372 + varZ * -0.4986;
-    let varG = varX * -0.9689 + varY * 1.8758 + varZ * 0.0415;
-    let varB = varX * 0.0557 + varY * -0.2040 + varZ * 1.0570;
-
-    if (varR > 0.0031308) {
-      varR = 1.055 * (Math.pow(varR, 1 / 2.4)) - 0.055;
-    } else {
-      varR = 12.92 * varR;
-    }
-    if (varG > 0.0031308) {
-      varG = 1.055 * (Math.pow(varR, 1 / 2.4)) - 0.055;
-    } else {
-      varG = 12.92 * varG;
-    }
-    if (varB > 0.0031308) {
-      varB = 1.055 * (Math.pow(varR, 1 / 2.4)) - 0.055;
-    } else {
-      varB = 12.92 * varB;
-    }
-
-    const R = varR * 255;
-    const G = varG * 255;
-    const B = varB * 255;
-
-    return [R, G, B];
   }
 }
