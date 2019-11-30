853f6af2c59ac2a570dae6cac4ef07e09219cf0d
AntonVovk <archwolf@protonmail.com>
2019-11-30 22:26:27 +0200

    point.ts refactored

---
+++ src/app/services/affine-transformation/utils/point.ts
@@ -0,0 +1,33 @@
+import {Vector} from './vector';
+
+export class Point {
+
+  constructor(x: number, y: number) {
+    this._x = x;
+    this._y = y;
+  }
+
+  private _x: number;
+
+  get x(): number {
+    return this._x;
+  }
+
+  set x(value: number) {
+    this._x = value;
+  }
+
+  private _y: number;
+
+  get y(): number {
+    return this._y;
+  }
+
+  set y(value: number) {
+    this._y = value;
+  }
+
+  public toVector(): Vector {
+    return new Vector([this._x, this._y, 1]);
+  }
+}
