7d3a3bcac0b166eeb0461971d576d5939292f406
Anton Vovk <archwolf@protonmail.com>
2019-12-10 23:55:37 +0200

    asda

--- src/app/modules/tutorial/tutorial.component.ts
+++ src/app/modules/tutorial/tutorial.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tutorial',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialComponent implements OnInit {
 
-  constructor() { }
+  fractal = false;
+  color = false;
+  affine = false;
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
 
+  fractalClick() {
+    this.fractal = !this.fractal;
+  }
+
+  colorClick() {
+    this.color = !this.color;
+  }
+
+  affineClick() {
+    this.affine = !this.affine;
+  }
 }
